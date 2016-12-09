b7d2a8315de1bf01b0f697a592e286c77db151bf
h_ohta <h_ohta@ertl.jp>
2016-12-09 19:37:26 +0900

    create FizzBuzz

--- No9002/main.cpp
+++ No9002/main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
 
 int main() {
-  std::cout << "Hello World!" << std::endl;
+  int32_t N;
+  std::cin >> N;
+  if (N < 0) {
+    std::cout << "Please input number over 1" << std::endl;
+    exit(-1);
+  }
+
+  for (int32_t i = 1; i <= N; i++) {
+
+    if (i % 3 == 0 && i % 5 == 0)
+      std::cout << "FizzBuzz" << std::endl;
+    else if (i % 3 == 0)
+      std::cout << "Fizz" << std::endl;
+    else if (i % 5 == 0)
+      std::cout << "Buzz" << std::endl;
+    else
+      std::cout << i << std::endl;
+
+  }
   return 0;
 }
